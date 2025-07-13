b5fc5049022037bdff2f23bc81b6903225ef8fe1
Sanwar Hussain <work.sanwar.hussain@gmail.com>
2025-07-14 00:01:41 +0530

    removed basepath

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	basePath: process.env.NEXT_PUBLIC_API_BASE_URL,
 	images: {
 		remotePatterns: [
 			{
